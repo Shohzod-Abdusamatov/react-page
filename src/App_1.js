5a318c06acec02b49cfa3fd43c088bf5ac2edf55
Shohzod Abdusamatov <shohzodabdusamatov10@gmail.com>
2020-12-14 19:47:29 +0500

    page-transition-with-react
    my first react project.

---
+++ src/App_1.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { PageTransition } from "@steveeeie/react-page-transition";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import NotFoundPage from "./pages/PageNotFound";
+import Page from "react-page-loading";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+           <BrowserRouter>
+          <Route
+            render={({ location }) => {
+              return (
+                <PageTransition
+                  preset="cubeToTop"
+                  transitionKey={location.pathname}
+                >
+                  <Switch location={location}>
+                    <Route exact path="/" component={Home} />
+                    <Route  path="/about" component={About} />
+                    <Route path="/services" component={Services} />
+                    <Route component={NotFoundPage} />
+                  </Switch>
+                </PageTransition>
+              );
+            }}
+          />
+        </BrowserRouter>
+
+    </>
+  );
+}
